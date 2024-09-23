2a46d2e2ee9751980dc4e496d517575c04fa589c
HarrisonTCodes <htillbackup@gmail.com>
2024-09-23 18:57:34 +0100

    Changed card data structure to array-based

--- app/api/cards/route.ts
+++ app/api/cards/route.ts
@@ -1,39 +1,110 @@
 import { NextRequest } from "next/server";
 
-const cardData = {
-    "bodyguard": 4,
-    "deputy": 4,
-    "doctor": 4,
-    "investigator": 6,
-    "mayor": 8,
-    "medium": 3,
-    "sheriff": 7,
-    "survivor": 4,
-    "veteran": 3,
-    "vigilante": 5,
-    "townie": 1,
-    "peaceful townie": -1,
-    "spiteful townie": -1,
-    "politician": -2,
-    "godfather": -8,
-    "janitor": -8,
-    "mafioso": -6,
-    "consigliere": -10,
-    "blackmailer": -9,
-    "amnesiac": 0,
-    "executioner": -4,
-    "jester": -1,
-    "serial killer": -8,
-    "werewolf": -9,
-    "witch": -5
-}
+const cards = [
+    {
+        name: "bodyguard",
+        value: 4
+    },
+    {
+        name: "deputy",
+        value: 4
+    },
+    {
+        name: "doctor",
+        value: 4
+    },
+    {
+        name: "investigator",
+        value: 6
+    },
+    {
+        name: "mayor",
+        value: 8
+    },
+    {
+        name: "medium",
+        value: 3
+    },
+    {
+        name: "sheriff",
+        value: 7
+    },
+    {
+        name: "survivor",
+        value: 4
+    },
+    {
+        name: "veteran",
+        value: 3
+    },
+    {
+        name: "vigilante",
+        value: 5
+    },
+    {
+        name: "townie",
+        value: 1
+    },
+    {
+        name: "peaceful townie",
+        value: -1
+    },
+    {
+        name: "spiteful townie",
+        value: -1
+    },
+    {
+        name: "politician",
+        value: -2
+    },
+    {
+        name: "godfather",
+        value: -8
+    },
+    {
+        name: "janitor",
+        value: -8
+    },
+    {
+        name: "mafioso",
+        value: -6
+    },
+    {
+        name: "consigliere",
+        value: -10
+    },
+    {
+        name: "blackmailer",
+        value: -9
+    },
+    {
+        name: "amnesiac",
+        value: 0
+    },
+    {
+        name: "executioner",
+        value: -4
+    },
+    {
+        name: "jester",
+        value: -1
+    },
+    {
+        name: "serial killer",
+        value: -8
+    },
+    {
+        name: "werewolf",
+        value: -9
+    },
+    {
+        name: "witch",
+        value: -5
+    }
+]
 
 export function GET(request: NextRequest) {
     const prompt = request.nextUrl.searchParams.get("prompt") ?? ""
 
-    return Response.json(
-        Object.keys(cardData)
-            .filter(key => key.includes(prompt))
-            .reduce((res: any, key: string) => (res[key] = cardData[key as keyof typeof cardData], res), {})
-    )
+    return Response.json(cards.filter((card) => card.name.includes(prompt.toLowerCase())))
 }
