beadda4300f2d7a9dc71dd78b174bb34107da292
HarrisonTCodes <htillbackup@gmail.com>
2024-09-24 18:18:11 +0100

    Improvements to search options menu

--- app/components/search/SearchOption.tsx
+++ app/components/search/SearchOption.tsx
@@ -2,6 +2,6 @@ import { Card } from "@/app/interfaces/Card";
 
 export default function SearchOption({ name, value } : Card) {
     return (
-        <button className="text-lg border py-2">{name}</button>
+        <button className="text-lg border py-2" onClick={() => console.log(name)}>{name}</button>
     )
 }
--- app/components/search/SearchOptions.tsx
+++ app/components/search/SearchOptions.tsx
@@ -1,5 +1,6 @@
 import { Card } from "@/app/interfaces/Card";
 import SearchOption from "./SearchOption";
+import { useState } from "react";
 
 // Function to convert string into title format (sufficient for all cases here)
 function title(str: string) {
@@ -7,8 +8,15 @@ function title(str: string) {
 }
 
 export default function SearchOptions({ optionsState, visible } : { optionsState: Card[], visible: boolean }) {
+    // Flag for when options are hovered (and clicked), preventing options from closing and click not being registered
+    const [hovered, setHovered] = useState<boolean>(false)
+
     return (
-        <div className={`flex flex-col w-[90%] max-w-[500px] cursor-pointer z-1 absolute overflow-y-scroll max-h-60 mt-14 rounded-xl border-2 bg-white ${optionsState.length > 0 && visible ? "visible" : "invisible"}`}>
+        <div 
+            className={`flex flex-col w-[90%] max-w-[500px] cursor-pointer z-1 absolute overflow-y-scroll max-h-60 mt-14 rounded-xl border-2 bg-white ${optionsState.length > 0 && (visible || hovered) ? "visible" : "invisible"}`}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
             {optionsState.map((option: Card, index) => <SearchOption name={title(option.name)} value={option.value} key={index} />)}
         </div>
     )
