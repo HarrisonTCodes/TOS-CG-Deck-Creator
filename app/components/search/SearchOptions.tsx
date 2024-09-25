098cb470fae9215b8a1d2be55692897ee16638eb
HarrisonTCodes <htillbackup@gmail.com>
2024-09-25 16:31:21 +0100

    Made search options slightly transparent to improve UX

--- app/components/search/SearchOptions.tsx
+++ app/components/search/SearchOptions.tsx
@@ -8,7 +8,7 @@ export default function SearchOptions({ optionsState, visible } : { optionsState
 
     return (
         <div 
-            className={`flex flex-col w-[90%] max-w-[500px] cursor-pointer z-1 absolute overflow-y-scroll max-h-52 mt-14 rounded-xl border-2 bg-white ${optionsState.length > 0 && (visible || hovered) ? "visible" : "invisible"}`}
+            className={`flex flex-col w-[90%] max-w-[500px] cursor-pointer z-1 absolute overflow-y-scroll max-h-52 mt-14 rounded-xl border-2 bg-white bg-opacity-90 ${optionsState.length > 0 && (visible || hovered) ? "visible" : "invisible"}`}
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
         >
